61c737295585f58f4676cd55c0472469d5b652e3
Het <hra.opash@gmail.com>
2026-04-15 12:45:15 +0530

    if user window reload currentUser  value store using GetMe API

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import ProtectedRoutes from "./components/ProtectedRoutes";
+import { setCurrentUser, setCheckingAuth } from "./features/usersSlice";
+import apiRequest from "./utils/apiRequest";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const res = await apiRequest.get("/user");
+        dispatch(setCurrentUser(res.data));
+      } catch {
+        dispatch(setCheckingAuth(false));
+      }
+    };
+    checkAuth();
+  }, []);
+
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
